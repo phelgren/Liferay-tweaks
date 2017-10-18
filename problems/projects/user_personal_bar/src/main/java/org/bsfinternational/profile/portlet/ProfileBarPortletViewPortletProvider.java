c722b17c54626acfe111a9f8d6c5ec934aeb5222
Pete Helgren <pete@valadd.com>
2017-10-18 15:10:28 -0500

    Create ProfileBarPortletViewPortletProvider.java

---
+++ problems/projects/user_personal_bar/src/main/java/org/bsfinternational/profile/portlet/ProfileBarPortletViewPortletProvider.java
@@ -0,0 +1,27 @@
+package org.bsfinternational.profile.portlet;
+
+import org.bsfinternational.profile.constants.ProfileBarPortletKeys;
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.admin.kernel.util.PortalUserPersonalBarApplicationType;
+
+import com.liferay.portal.kernel.portlet.BasePortletProvider;
+import com.liferay.portal.kernel.portlet.ViewPortletProvider;
+
+@Component(
+		immediate = true,
+		property = {
+			"model.class.name=" + PortalUserPersonalBarApplicationType.UserPersonalBar.CLASS_NAME
+		},
+		service = ViewPortletProvider.class
+	)
+
+public class ProfileBarPortletViewPortletProvider 
+extends BasePortletProvider implements ViewPortletProvider{
+	
+	@Override
+	public String getPortletName() {
+		return ProfileBarPortletKeys.ProfileBar;
+	}
+	
+}
