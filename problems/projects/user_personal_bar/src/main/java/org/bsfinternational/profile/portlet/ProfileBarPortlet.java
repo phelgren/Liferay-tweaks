80e1b54a3f2b421ccd51a8f98a837f4bdff0f753
Pete Helgren <pete@valadd.com>
2017-10-24 09:31:58 -0500

    Updated to final working code

--- problems/projects/user_personal_bar/src/main/java/org/bsfinternational/profile/portlet/ProfileBarPortlet.java
+++ problems/projects/user_personal_bar/src/main/java/org/bsfinternational/profile/portlet/ProfileBarPortlet.java
@@ -1,26 +1,29 @@
 package org.bsfinternational.profile.portlet;
 
-import org.bsfinternational.profile.constants.ProfileBarPortletKeys;
-import org.bsfinternational.profile.constants.ProfileBarPortletWebKeys;
-
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.util.bridges.freemarker.FreeMarkerPortlet;
-import com.liferay.application.list.PanelAppRegistry;
-import com.liferay.application.list.PanelCategoryRegistry;
-import com.liferay.application.list.constants.PanelCategoryKeys;
-import com.liferay.application.list.display.context.logic.PanelCategoryHelper;
-
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.servlet.http.HttpServletRequest;
 
+import org.bsfinternational.profile.constants.ProfileBarPortletKeys;
+import org.bsfinternational.profile.constants.ProfileBarPortletWebKeys;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.application.list.PanelAppRegistry;
+import com.liferay.application.list.PanelCategoryRegistry;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.application.list.display.context.logic.PanelCategoryHelper;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.util.bridges.freemarker.FreeMarkerPortlet;
 
 /**
  * @author Pete
@@ -36,20 +39,35 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
 		"com.liferay.portlet.use-default-template=false",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=User Personal Bar",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/templates/view.ftl",
 		"javax.portlet.name=" + ProfileBarPortletKeys.ProfileBar,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user",
-		"service.ranking:Integer=20"
+		"javax.portlet.security-role-ref=power-user,user"
 	},
 	service = Portlet.class
 )
 public class ProfileBarPortlet extends FreeMarkerPortlet {
+	
+	@Override
+	public void render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+		
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		User user = themeDisplay.getUser();
+
+		if (!user.isDefaultUser()) {
+			renderRequest.setAttribute(
+				ProfileBarPortletWebKeys.NOTIFICATIONS_COUNT,
+				getNotificationsCount(themeDisplay));
+		}
+
+		super.render(renderRequest, renderResponse);
+	}
 
 	protected int getNotificationsCount(ThemeDisplay themeDisplay) {
 		PanelCategoryHelper panelCategoryHelper = new PanelCategoryHelper(
